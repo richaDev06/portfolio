2278016bdfd13090e74563e229ebfb981739709a
richaDev06 <itsricha4work@gmail.com>
2025-08-13 21:47:48 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# 🌟 My Portfolio
+
+Welcome to my personal portfolio website!  
+This site showcases my projects, skills, and experiences as a developer.
+
+## 🔗 Live Demo
+[View Portfolio](https://your-live-link-here.com)
+
+## 📌 Features
+- Responsive design for all devices 📱💻
+- Smooth animations ✨
+- Interactive navigation menu 📂
+- Showcases projects with github repository 🔗
+
+## 🛠️ Tech Stack
+- **HTML5**
+- **CSS3**
+- **JavaScript**
---
+++ script.js
@@ -0,0 +1,57 @@
+// toggle icon bar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+}
+
+//scroll section active link
+
+let section = document.querySelector('.section');
+let navLinks = document.querySelector('header nav a');
+
+window.onscroll = ()=>{
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach.apply(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            })
+        }
+    });
+
+    //sticky navbar
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky' , window.scrollY > 100);
+
+    //toggle icon navbar
+    menuIcon.classList.remove('fa-xmark');
+}
+
+//scroll reveal
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+})
+
+ScrollReveal().reveal('.home-content , heading' , {origin : 'top'})
+ScrollReveal().reveal('.home-img , .skills-container , projects-box , .contact form' , {origin : 'bottom'})
+ScrollReveal().reveal('.home-contact h1 , .about-img' , {origin: 'left'})
+ScrollReveal().reveal('.home-contact p , .about-content' , {origin: 'right'})
+
+//typed js
+const tped = new Typed('.multiple-text',{
+    strings: ['Explorer' , 'Developer' ,'Learner'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true
+})
